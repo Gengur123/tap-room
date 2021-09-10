e8793b2b979a2f4a55b1f7b92e9b6588178e8255
Gengur123 <meleearthur@gmail.com>
2021-09-10 09:49:25 -0700

    Add filler text

---
+++ src/components/HeaderBlock.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HeaderBlock() {
+  return (
+    <>
+      <h1>Welcome to the Tap Room</h1>
+    </>
+  );
+}
+
+export default HeaderBlock;
---
+++ src/components/KegBlock.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function KegBlock() {
+  return (
+    <React.Fragment>
+      <h1>KegBlock</h1>
+    </React.Fragment>
+  );
+}
+
+export default KegBlock;
