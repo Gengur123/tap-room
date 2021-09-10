a89968bf764a05266289cb54fdfa8b82c62b086e
Gengur123 <meleearthur@gmail.com>
2021-09-10 11:43:59 -0700

    Fixed buggit add .

--- src/components/KegBlock.jsx
+++ src/components/KegBlock.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function KegBlock() {
-  return (
-    <React.Fragment>
-      <h1>KegBlock</h1>
-    </React.Fragment>
-  );
+  return <React.Fragment></React.Fragment>;
 }
+
+KegBlock.propTypes = {
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  pintsLeft: PropTypes.number.isRequired,
+  alcoholContent: PropTypes.number.isRequired,
+};
 
 export default KegBlock;
