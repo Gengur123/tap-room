c2a893d773af01e5144a109bf40780eaf19c3512
Gengur123 <meleearthur@gmail.com>
2021-09-10 10:15:32 -0700

    Move content around

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeaderBlock from "./HeaderBlock";
+import KegControl from "./KegControl";
+
+function App() {
+  return (
+    <>
+      <HeaderBlock />
+      <KegControl />
+    </>
+  );
+}
+
+export default App;
